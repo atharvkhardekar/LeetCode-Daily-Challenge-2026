05dc634876b3d8be367109a8d272eb812dbd3504
atharvkhardekar <khardekaratharv@gmail.com>
2026-01-04 23:37:08 +0530

    04/01/2026 - LeetCode(1390)

---
+++ Jan/04_Four_Divisors.cpp
@@ -0,0 +1,35 @@
+// 1390. Four Divisors
+// Daily Challenge - 04/01/2026
+
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+
+        for (int num : nums) {
+            int cnt = 0, sum = 0;
+
+            for (int i = 1; i * i <= num; i++) {
+                if (num % i == 0) {
+                    int d1 = i;
+                    int d2 = num / i;
+
+                    cnt++;
+                    sum += d1;
+
+                    if (d1 != d2) {
+                        cnt++;
+                        sum += d2;
+                    }
+
+                    if (cnt > 4) break;
+                }
+            }
+
+            if (cnt == 4)
+                ans += sum;
+        }
+
+        return ans;
+    }
+};
