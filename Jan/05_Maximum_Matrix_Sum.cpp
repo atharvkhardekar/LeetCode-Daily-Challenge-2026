763b9bc99791db416f70386c47e99aa6e67fcad2
atharvkhardekar <khardekaratharv@gmail.com>
2026-01-05 22:57:22 +0530

    05/01/2026 - LeetCode(1975)

---
+++ Jan/05_Maximum_Matrix_Sum.cpp
@@ -0,0 +1,25 @@
+// 1975. Maximum Matrix Sum
+// Daily Challenge - 04/01/2026
+
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long sum = 0;
+        int negCount = 0;
+        int minAbs = INT_MAX;
+
+        for (auto &row : matrix) {
+            for (int val : row) {
+                if (val < 0) negCount++;
+                sum += abs(val);
+                minAbs = min(minAbs, abs(val));
+            }
+        }
+
+        if (negCount % 2 == 1) {
+            sum -= 2LL * minAbs;
+        }
+
+        return sum;
+    }
+};
