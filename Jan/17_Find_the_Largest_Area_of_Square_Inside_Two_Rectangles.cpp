183ab9fccb52b3a7ec5e369fb37da9ad229ccc56
atharvkhardekar <khardekaratharv@gmail.com>
2026-01-18 21:41:55 +0530

    17/01/2026 - LeetCode(3047)

---
+++ Jan/17_Find_the_Largest_Area_of_Square_Inside_Two_Rectangles.cpp
@@ -0,0 +1,32 @@
+// 3047. Find the Largest Area of Square Inside Two Rectangles
+// Daily Challenge - 17/01/2026
+
+class Solution {
+public:
+    long long largestSquareArea(vector<vector<int>>& bottomLeft,
+                                vector<vector<int>>& topRight) {
+
+        int n = bottomLeft.size();
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                long long left   = max(bottomLeft[i][0], bottomLeft[j][0]);
+                long long right  = min(topRight[i][0],   topRight[j][0]);
+                long long bottom = max(bottomLeft[i][1], bottomLeft[j][1]);
+                long long top    = min(topRight[i][1],   topRight[j][1]);
+
+                long long width  = right - left;
+                long long height = top - bottom;
+
+                if (width > 0 && height > 0) {
+                    long long side = min(width, height);
+                    ans = max(ans, side * side);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
