8d5c77f102a8411323433a0140cdbb852abb6d5c
atharvkhardekar <khardekaratharv@gmail.com>
2026-01-26 21:32:14 +0530

    26/01/2026 - LeetCode(1200)

---
+++ Jan/26_Minimum_Absolute_Difference.cpp
@@ -0,0 +1,26 @@
+// 1200. Minimum Absolute Difference
+// Daily Challenge - 26/01/2026
+
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        
+        int n = arr.size();
+        int minDiff = INT_MAX;
+        
+        for (int i = 1; i < n; i++) {
+            minDiff = min(minDiff, arr[i] - arr[i - 1]);
+        }
+        
+        vector<vector<int>> result;
+        
+        for (int i = 1; i < n; i++) {
+            if (arr[i] - arr[i - 1] == minDiff) {
+                result.push_back({arr[i - 1], arr[i]});
+            }
+        }
+        
+        return result;
+    }
+};
