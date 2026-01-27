f042d79252efad87ba5801ed97ff60ec70cf38b5
atharvkhardekar <khardekaratharv@gmail.com>
2026-01-27 22:42:30 +0530

    27/01/2026 - LeetCode(3650)

---
+++ Jan/27_Minimum_Cost_Path_with_Edge_Reversals.cpp
@@ -0,0 +1,37 @@
+// 3650. Minimum Cost Path with Edge Reversals
+// Daily Challenge - 27/01/2026
+
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> graph(n);
+
+        for (auto &e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            graph[u].push_back({v, w});        
+            graph[v].push_back({u, 2 * w});    
+        }
+
+        vector<long long> dist(n, LLONG_MAX);
+        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<>> pq;
+
+        dist[0] = 0;
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+            auto [cost, u] = pq.top();
+            pq.pop();
+
+            if (cost > dist[u]) continue;
+
+            for (auto &[v, w] : graph[u]) {
+                if (dist[v] > cost + w) {
+                    dist[v] = cost + w;
+                    pq.push({dist[v], v});
+                }
+            }
+        }
+
+        return dist[n-1] == LLONG_MAX ? -1 : dist[n-1];
+    }
+};
