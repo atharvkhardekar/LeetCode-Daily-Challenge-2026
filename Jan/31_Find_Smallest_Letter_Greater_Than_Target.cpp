36d9e82cb9955e9ef3cebc25d5dc64e6f96e1aa7
atharvkhardekar <khardekaratharv@gmail.com>
2026-01-31 22:54:05 +0530

    31/01/2026 - LeetCode(744)

---
+++ Jan/31_Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -0,0 +1,23 @@
+// 744. Find Smallest Letter Greater Than Target
+// Daily Challenge - 31/01/2026
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int left = 0, right = letters.size() - 1;
+        char ans = letters[0];  
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (letters[mid] > target) {
+                ans = letters[mid];
+                right = mid - 1;  
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
