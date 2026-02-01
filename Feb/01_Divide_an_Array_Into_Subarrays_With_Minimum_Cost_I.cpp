93405b21f9f2889bbe34e4cf323386bdf61e102b
atharvkhardekar <khardekaratharv@gmail.com>
2026-02-01 22:12:18 +0530

    01/02/2026 - LeetCode(3010)

---
+++ Feb/01_Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I.cpp
@@ -0,0 +1,22 @@
+// 3010. Divide an Array Into Subarrays With Minimum Cost I
+// Daily Challenge - 01/02/2026
+
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int first = nums[0];
+        
+        int min1 = INT_MAX, min2 = INT_MAX;
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+        
+        return first + min1 + min2;
+    }
+};
