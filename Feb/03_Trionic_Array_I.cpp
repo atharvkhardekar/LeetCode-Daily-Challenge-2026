cc902bd4fcb0ba4616ebedfb139d833db7d431c7
atharvkhardekar <khardekaratharv@gmail.com>
2026-02-04 22:40:56 +0530

    03/02/2026 - LeetCode(3637)

---
+++ Feb/03_Trionic_Array_I.cpp
@@ -0,0 +1,31 @@
+// 3637. Trionic Array I
+// Daily Challenge - 03/02/2026
+
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) return false;
+
+        int i = 0;
+
+        while (i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+        if (i == 0 || i == n - 1) return false; 
+
+        
+        int p = i;
+        while (i + 1 < n && nums[i] > nums[i + 1]) {
+            i++;
+        }
+        if (i == p || i == n - 1) return false; 
+
+      
+        while (i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+
+        return i == n - 1;
+    }
+};
