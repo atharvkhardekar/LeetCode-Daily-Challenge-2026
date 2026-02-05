99e1078f29e21f72c3abab4074e3df2fe0987081
atharvkhardekar <khardekaratharv@gmail.com>
2026-02-05 23:14:24 +0530

    05/02/2026 - LeetCode(3379)

---
+++ Feb/05_Transformed_Array.cpp
@@ -0,0 +1,25 @@
+// 3379. Transformed Array
+// Daily Challenge - 05/02/2026
+
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                result[i] = 0;
+            } else {
+                int newIndex = (i + nums[i]) % n;
+                
+                if (newIndex < 0)
+                    newIndex += n;
+                
+                result[i] = nums[newIndex];
+            }
+        }
+
+        return result;
+    }
+};
