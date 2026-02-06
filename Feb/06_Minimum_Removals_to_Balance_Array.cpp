e85956d7595fc4c6d2fbd6bf84f22dfb6edfdd56
atharvkhardekar <khardekaratharv@gmail.com>
2026-02-06 22:23:11 +0530

    06/02/2026 - LeetCode(3634)

---
+++ Feb/06_Minimum_Removals_to_Balance_Array.cpp
@@ -0,0 +1,22 @@
+// 3634. Minimum Removals to Balance Array
+// Daily Challenge - 06/02/2026
+
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        int l = 0;
+        int maxLen = 1;
+        
+        for (int r = 0; r < n; r++) {
+            while ((long long)nums[r] > (long long)nums[l] * k) {
+                l++;
+            }
+            maxLen = max(maxLen, r - l + 1);
+        }
+        
+        return n - maxLen;
+    }
+};
