f8fface709135e00baf16a15d3021cb9c31587c9
atharvkhardekar <khardekaratharv@gmail.com>
2026-02-12 22:42:49 +0530

    12/02/2026 - LeetCode(3713)

---
+++ Feb/12_Longest_Balanced_Substring_I.cpp
@@ -0,0 +1,33 @@
+// 3713. Longest Balanced Substring I
+// Daily Challenge - 12/02/2026
+
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int n = s.size();
+        int ans = 1; 
+        
+        for (int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);
+            
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+                
+                int mn = INT_MAX, mx = 0;
+                
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        mn = min(mn, freq[k]);
+                        mx = max(mx, freq[k]);
+                    }
+                }
+                
+                if (mn == mx) {
+                    ans = max(ans, j - i + 1);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
