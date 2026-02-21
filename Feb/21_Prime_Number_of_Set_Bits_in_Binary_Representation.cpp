fe0c6c27771ecbc18a09e22be1f1c8f1c3114a18
atharvkhardekar <khardekaratharv@gmail.com>
2026-02-21 22:38:36 +0530

    21/02/2026 - LeetCode(762)

---
+++ Feb/21_Prime_Number_of_Set_Bits_in_Binary_Representation.cpp
@@ -0,0 +1,18 @@
+// 762. Prime Number of Set Bits in Binary Representation
+// Daily Challenge - 21/02/2026
+
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        unordered_set<int> primes = {2, 3, 5, 7, 11, 13, 17, 19};
+
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            int bits = __builtin_popcount(i); 
+            if (primes.count(bits)) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
