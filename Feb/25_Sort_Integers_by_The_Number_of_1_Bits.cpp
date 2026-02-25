5efc95ec8ab5b67ef3fe722ea4203fa35a780e20
atharvkhardekar <khardekaratharv@gmail.com>
2026-02-25 22:35:10 +0530

    25/02/2026 - LeetCode(1356)

---
+++ Feb/25_Sort_Integers_by_The_Number_of_1_Bits.cpp
@@ -0,0 +1,19 @@
+// 1356. Sort Integers by The Number of 1 Bits
+// Daily Challenge - 25/02/2026
+
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [](int a, int b) {
+            int bitsA = __builtin_popcount(a);
+            int bitsB = __builtin_popcount(b);
+            
+            if (bitsA == bitsB)
+                return a < b;     
+            
+            return bitsA < bitsB;  
+        });
+        
+        return arr;
+    }
+};
