10c5036d498a648bc40ce445a8fcbcf17e57c205
atharvkhardekar <khardekaratharv@gmail.com>
2026-03-25 23:10:48 +0530

    25/03/2026 - LeetCode(3546)

---
+++ Mar/25_Equal_Sum_Grid_Partition_I.cpp
@@ -0,0 +1,36 @@
+// 3546. Equal Sum Grid Partition I
+// Daily Challenge - 25/03/2026
+
+class Solution {
+public:
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        
+        long long total = 0;
+        for (auto &row : grid)
+            for (auto val : row)
+                total += val;
+
+        if (total % 2 != 0) return false;
+
+        long long target = total / 2;
+
+        long long sum = 0;
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += grid[i][j];
+            }
+            if (sum == target) return true;
+        }
+
+        sum = 0;
+        for (int j = 0; j < n - 1; j++) {
+            for (int i = 0; i < m; i++) {
+                sum += grid[i][j];
+            }
+            if (sum == target) return true;
+        }
+
+        return false;
+    }
+};
