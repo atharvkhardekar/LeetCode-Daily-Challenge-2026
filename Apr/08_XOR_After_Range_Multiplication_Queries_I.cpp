44e39d36652af72d226c620bf4d4954bb024fcda
atharvkhardekar <khardekaratharv@gmail.com>
2026-04-08 23:13:43 +0530

    08/04/2026 - LeetCode(3653)

---
+++ Apr/08_XOR_After_Range_Multiplication_Queries_I.cpp
@@ -0,0 +1,24 @@
+// 3653. XOR After Range Multiplication Queries I
+// Daily Challenge - 08/04/2026
+
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        const int MOD = 1e9 + 7;
+
+        for (auto &q : queries) {
+            int l = q[0], r = q[1], k = q[2], v = q[3];
+
+            for (int idx = l; idx <= r; idx += k) {
+                nums[idx] = (1LL * nums[idx] * v) % MOD;
+            }
+        } 
+
+        int res = 0;
+        for (int x : nums) {
+            res ^= x;
+        }
+
+        return res;
+    }
+};
