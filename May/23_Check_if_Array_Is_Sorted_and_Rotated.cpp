445576f4593ca48977728eb7f39d7268e2030d6b
atharvkhardekar <khardekaratharv@gmail.com>
2026-05-23 23:07:50 +0530

    23/05/2026 - LeetCode(1752)

---
+++ May/23_Check_if_Array_Is_Sorted_and_Rotated.cpp
@@ -0,0 +1,17 @@
+// 1752. Check if Array Is Sorted and Rotated
+// Daily Challenge - 23/05/2026
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+};
