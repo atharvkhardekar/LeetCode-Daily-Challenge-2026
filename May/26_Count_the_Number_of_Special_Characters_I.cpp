c11ebb491245b5d453180daf294e3b9920eaa11f
atharvkhardekar <khardekaratharv@gmail.com>
2026-05-26 23:13:46 +0530

    26/05/2026 - LeetCode(3120)

---
+++ May/26_Count_the_Number_of_Special_Characters_I.cpp
@@ -0,0 +1,25 @@
+// 3120. Count the Number of Special Characters I
+// Daily Challenge - 26/05/2026
+
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_set<char> lower, upper;
+
+        for(char ch : word) {
+            if(islower(ch))
+                lower.insert(ch);
+            else
+                upper.insert(tolower(ch));
+        }
+
+        int count = 0;
+
+        for(char ch : lower) {
+            if(upper.count(ch))
+                count++;
+        }
+
+        return count;
+    }
+};
