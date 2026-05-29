9aefad03dbd63178fb702d1a532cf125a0d337d1
atharvkhardekar <khardekaratharv@gmail.com>
2026-05-29 23:40:47 +0530

    29/05/2026 - LeetCode(3300)

---
+++ May/29_Minimum_Element_After_Replacement_With_Digit_Sum.cpp
@@ -0,0 +1,27 @@
+// 3300. Minimum Element After Replacement With Digit Sum
+// Daily Challenge - 29/05/2026
+
+class Solution {
+public:
+    
+    int digitSum(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    int minElement(vector<int>& nums) {
+        int ans = INT_MAX;
+
+        for (int num : nums) {
+            ans = min(ans, digitSum(num));
+        }
+
+        return ans;
+    }
+};
